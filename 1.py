2a7459e47ef6b9786982f3182231f48dbe6cd752
sonya <sonya@>
2020-02-21 17:46:31 +0300

    3

--- 1.py
+++ 1.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def default():
-    return ''
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
 
 
 @app.route('/promotion')
@@ -16,6 +21,23 @@ def promotion():
         'Присоединяйся!'
     ]
     return '</br>'.join(message)
+
+
+@app.route('/image_mars')
+def image_mars():
+    return """<!doctype html>
+                <html lang="ru">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{}" alt="здесь должна была быть картинка, 
+                        но не нашлась" width="450" height="450"/>
+                        <p>Вот она такая, красная планета.</p>
+                  </body>
+                </html>""".format(url_for('static', filename='img/mars.png'))
 
 
 if __name__ == '__main__':
